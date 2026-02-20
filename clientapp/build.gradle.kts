5d3f4a949d07463239c7450fcc420b9f56c9366e
Swapnil Khare <swapnildroid@users.noreply.github.com>
2026-02-20 07:25:02 +0530

    Revert "Library module"
    This reverts commit 801cc462bbb23a309a867e9c1867782cc45bfed5.

--- clientapp/build.gradle.kts
+++ clientapp/build.gradle.kts
@@ -51,7 +51,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(project(":mylibrary"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
