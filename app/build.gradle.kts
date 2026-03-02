664ef79dfb66980a0e9a6765aab75dc96be83f58
Swapnil Khare <swapnildroid@users.noreply.github.com>
2026-03-02 13:56:24 +0530

    Using parcelize annotation.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("com.google.devtools.ksp") version "2.0.21-1.0.25"
+    id("kotlin-parcelize")
 }
 
 android {
--- app/src/main/java/com/example/aidlprintoutservice/MyData.kt
+++ app/src/main/java/com/example/aidlprintoutservice/MyData.kt
@@ -1,41 +1,10 @@
-package com.example.aidlprintoutservice;
+package com.example.aidlprintoutservice
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
-// src/main/java/com/example/MyData.java
-public class MyData implements Parcelable {
-    public String name;
-    public int age;
-
-    public MyData(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    protected MyData(Parcel in) {
-        name = in.readString();
-        age = in.readInt();
-    }
-
-    @Override
-    public String toString() {
-        return "MyData{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(name);
-        dest.writeInt(age);
-    }
-
-    @Override public int describeContents() { return 0; }
-
-    public static final Creator<MyData> CREATOR = new Creator<>() {
-        @Override public MyData createFromParcel(Parcel in) { return new MyData(in); }
-        @Override public MyData[] newArray(int size) { return new MyData[size]; }
-    };
-}
+@Parcelize
+data class MyData(
+    var name: String,
+    var age: Int
+) : Parcelable
